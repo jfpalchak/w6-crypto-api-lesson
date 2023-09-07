cdb3c06b3c093bcffca4a49347ad9157ba9b2377
Joey Palchak <joey.palchak@gmail.com>
2023-09-07 11:01:45 -0700

    Add functions and handlers for new coin service.

--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@ import CryptoService from './js/crypto-service.js';
 
 // BUSINESS LOGIC
 
+// handle calling CoinGecko API endpoint for market's top coins (set to 10)
 async function getCoins() {
   // Utilizing ASYNC & AWAIT
   const response = await CryptoService.getCoins();
+  // const response = 'Whoops!';
   if (response[0]) {
     displayTopCoins(response);
   } else {
@@ -16,35 +18,95 @@ async function getCoins() {
 
 }
 
+// handle calling CoinGecko API endpoint for a specific coin
+async function getCoinInfo(coin) {
+
+  const response = await CryptoService.getCoinInfo(coin);
+  // const response = 'Whoops!';
+  if (response.id) {
+    displayCoinInfo(response);
+  } else {
+    displayCoinError(response);
+  }
+}
+
 // UI LOGIC
-// Display the API response for the top ten crypto coins
-function displayTopCoins(topTen) {
-  const topTenDiv = document.querySelector('#top-ten-display');
-  const ol = document.createElement('ol');
 
-  topTen.forEach(function(coinObject) {
-    let coin = coinObject.name;
-    let currentPrice = coinObject["current_price"];
-    let string = `${coin}, currently listed at ${currentPrice}`;
+// Display the API response for the specified crypto coin
+function displayCoinInfo(coin) {
+  const coinDiv = document.querySelector('#coin-info-display');
+  const purseDiv = document.querySelector('div#hidden');
+  if (purseDiv) {
+    purseDiv.removeAttribute('id');
+  }
 
-    let li = document.createElement('li');
-    li.append(string);
-    ol.append(li);
-  });
+  coinDiv.innerText = null;
 
-  topTenDiv.append(ol);
+  const header = document.createElement('h3');
+  const img = document.createElement('img');
+  const description = document.createElement('p');
+
+  header.innerHTML = coin.name;
+  img.src = coin.image.small;
+  img.alt = `${coin.name} icon`;
+  description.innerHTML = coin.description.en;
+
+  coinDiv.append(img, header, description);
 
 }
 
+// Display the API response for the top ten crypto coins
+function displayTopCoins(topTen) {
+  const topTenDiv = document.querySelector('#top-ten-display');
+  const table = document.querySelector('table');
+
+  topTen.forEach(function(coinObject) {
+    let tRow = document.createElement('tr');
+
+    let td0 = document.createElement('td');
+    let td1 = document.createElement('td');
+    let td2 = document.createElement('td');
+    td1.setAttribute('id', `${coinObject.id}`);
+
+    td0.innerText = coinObject['market_cap_rank'];
+    td1.innerText = coinObject.name;
+    td2.innerText = coinObject["current_price"];
+
+    tRow.append(td0, td1, td2);
+    table.append(tRow);
+  });
+
+  topTenDiv.append(table);
+
+}
+
+// Display API error, if received
 function displayError(error) {
   const topTenDiv = document.querySelector('#top-ten-display');
   topTenDiv.innerText = `Whoops! ${error}`;
 }
 
+// Display API error, if received
+function displayCoinError(error) {
+  const coinDiv = document.querySelector('#coin-info-display');
+  coinDiv.innerText = `Whoops! ${error}`;
+}
 
+// handle all UI logic
 function handleEverything() {
 
+  // display the market's current top ten coins
   getCoins();
+
+  // Event handler for displaying more details for a specific coin
+  document.querySelector('table').addEventListener('click', (e) => {
+    const clickTarget = e.target.id;
+
+    if (clickTarget) {
+      getCoinInfo(clickTarget);
+    }
+
+  });
   
 }
 
